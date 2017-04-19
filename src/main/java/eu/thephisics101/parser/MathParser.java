f55c00c7b8b84f1503529fe1ba1811be1a2474c0
ThePhisics101 <thephisics101@gmail.com>
2017-04-19 21:21:28 +0300

    Initial Commit

---
+++ src/main/java/eu/thephisics101/parser/MathParser.java
@@ -0,0 +1,12 @@
+package eu.thephisics101.parser;
+
+/**
+ * Created by ThePhisics101 on 19/04/2017.
+ *
+ */
+public class MathParser {
+
+    public double parser(String input) {
+        return 0;
+    }
+}
