53d75d2256e5aa11a079fd4a04adb973ef877b93
ThePhisics101 <thephisics101@gmail.com>
2017-04-23 00:12:11 +0300

    Added parser exception

---
+++ src/main/java/eu/thephisics101/parser/ParserException.java
@@ -0,0 +1,14 @@
+package eu.thephisics101.parser;
+
+/**
+ * Created by ThePhisics101 on 23/04/2017.
+ *
+ * Parser Exception
+ */
+public class ParserException extends Exception {
+
+    public ParserException(String message) {
+        super(message);
+    }
+
+}
